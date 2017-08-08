26d3e8dc737b0de18123a892c028eabb5e980088
Guilherme Moura Nascimento <gmoura.nas@gmail.com>
2017-08-08 13:30:04 -0300

    Working With JSON

--- playground/json.js
+++ playground/json.js
@@ -1,7 +1,22 @@
-var obj = {
-  name: 'Guilherme'
-};
+// var obj = {
+//   name: 'Guilherme'
+// };
 
-var stringObj = JSON.stringfy(obj);
-console.log(typeof stringObj);
-console.log(stringObj);
+// var stringObj = JSON.stringify(obj);
+// console.log(typeof stringObj);
+// console.log(stringObj);
+
+const fs = require('fs');
+
+var originaNote = {
+  title: 'Some title',
+  body: 'Some body'
+}
+
+var originalNoteString = JSON.stringify(originaNote)
+fs.writeFileSync('notes.json', originalNoteString);
+
+var noteString = fs.readFileSync('notes.json');
+var note = JSON.parse(noteString);
+console.log(typeof notes);
+console.log(note.title);
